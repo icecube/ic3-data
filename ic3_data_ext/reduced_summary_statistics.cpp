dcc888777a1b087873038c8b98ce736bf76dd471
mhuen <mhuen@users.noreply.github.com>
2020-10-17 23:56:45 +0200

    split file in two

--- ic3_data_ext/reduced_summary_statistics.cpp
+++ ic3_data_ext/reduced_summary_statistics.cpp
@@ -165,29 +165,18 @@ inline void fill_reduced_summary_statistics_data(
     bn::ndarray global_time_offset_batch = boost::python::extract<bn::ndarray>(
         container.attr("global_time_offset_batch"));
 
-    // define numpy arrays
-    bn::ndarray& x_dom;
-    bn::ndarray& x_dom_exclusions;
-    bn::ndarray& x_ic78;
-    bn::ndarray& x_ic78_exclusions;
-    bn::ndarray& x_deepcore;
-    bn::ndarray& x_deepcore_exclusions;
-
-    if (is_str_dom_format){
-        x_dom = boost::python::extract<bn::ndarray>(
-            container.attr("x_dom"));
-        x_dom_exclusions = boost::python::extract<bn::ndarray>(
-            container.attr("x_dom_exclusions"));
-    }else{
-        x_ic78 = boost::python::extract<bn::ndarray>(
-            container.attr("x_ic78"));
-        x_ic78_exclusions = boost::python::extract<bn::ndarray>(
-            container.attr("x_ic78_exclusions"));
-        x_deepcore = boost::python::extract<bn::ndarray>(
-            container.attr("x_deepcore"));
-        x_deepcore_exclusions = boost::python::extract<bn::ndarray>(
-            container.attr("x_deepcore_exclusions"));
-    }
+    bn::ndarray x_dom = boost::python::extract<bn::ndarray>(
+        container.attr("x_dom"));
+    bn::ndarray x_dom_exclusions = boost::python::extract<bn::ndarray>(
+        container.attr("x_dom_exclusions"));
+    bn::ndarray x_ic78 = boost::python::extract<bn::ndarray>(
+        container.attr("x_ic78"));
+    bn::ndarray x_ic78_exclusions = boost::python::extract<bn::ndarray>(
+        container.attr("x_ic78_exclusions"));
+    bn::ndarray x_deepcore = boost::python::extract<bn::ndarray>(
+        container.attr("x_deepcore"));
+    bn::ndarray x_deepcore_exclusions = boost::python::extract<bn::ndarray>(
+        container.attr("x_deepcore_exclusions"));
     // -------------------------------------------------------------
 
     // create a dict for the output data
